f7ceeb70d23d9cd157e4aa604adb787253e66a11
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2021-08-23 18:19:59 +1000

    Add grid to book form

--- FrontEnd/bookeroo/src/components/AddBookForm.js
+++ FrontEnd/bookeroo/src/components/AddBookForm.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import {useFormik} from 'formik';
-import {TextField} from '@material-ui/core';
+import React from "react";
+import { useFormik } from "formik";
+import { TextField, Grid } from "@material-ui/core";
 
 function inputProps(formik, name, label) {
-    return {label, name, onChange: formik.onChange, value: formik.values[name], onBlur: formik.onBlur, error: formik.errors[name], helperText: formik.errors[name]}
+  return {
+    label,
+    name,
+    onChange: formik.onChange,
+    value: formik.values[name],
+    onBlur: formik.onBlur,
+    error: formik.errors[name],
+    helperText: formik.errors[name],
+  };
 }
 
 /**
@@ -11,26 +19,34 @@ function inputProps(formik, name, label) {
  * @note This form is not for the sale pages, just the definition of a book, with title author etc.
  */
 export default function AddBookForm() {
-    const formik = useFormik({
-        initialValues: {
-            bookTitle: "",
-            author: "",
-            publisher: "",
-            publishDate: "",
-            isbn: "",
-            coverArtURL: "",
-            tableOfContents: "",
-        },
-        onSubmit() {},
-    });
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <TextField {...inputProps(formik, "Book Title", "bookTitle")} />
-            <TextField {...inputProps(formik, "Author", "author")} />
-            <TextField {...inputProps(formik, "Publisher", "publisher")} />
-            <TextField {...inputProps(formik, "Publish Date", "publishDate")} type="date" />
-            <TextField {...inputProps(formik, "ISBN", "isbn")} />
-            <TextField {...inputProps(formik, "Table of Contents", "tableOfContents")} multiline />
-        </form>
-    )
+  const formik = useFormik({
+    initialValues: {
+      bookTitle: "",
+      author: "",
+      publisher: "",
+      publishDate: "",
+      isbn: "",
+      coverArtURL: "",
+      tableOfContents: "",
+    },
+    onSubmit() {},
+  });
+  return (
+    <Grid>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField {...inputProps(formik, "Book Title", "bookTitle")} />
+        <TextField {...inputProps(formik, "Author", "author")} />
+        <TextField {...inputProps(formik, "Publisher", "publisher")} />
+        <TextField
+          {...inputProps(formik, "Publish Date", "publishDate")}
+          type="date"
+        />
+        <TextField {...inputProps(formik, "ISBN", "isbn")} />
+        <TextField
+          {...inputProps(formik, "Table of Contents", "tableOfContents")}
+          multiline
+        />
+      </form>
+    </Grid>
+  );
 }
