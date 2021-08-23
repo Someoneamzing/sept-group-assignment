94a5abb8bb27b5dfc1fe38b3627521067ec71628
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2021-08-23 17:52:13 +1000

    Add book form, fix gitignore

---
+++ FrontEnd/bookeroo/src/components/AddBookForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {useFormik} from 'formik';
+import {TextField} from '@material-ui/core';
+
+function inputProps(formik, name, label) {
+    return {label, name, onChange: formik.onChange, value: formik.values[name], onBlur: formik.onBlur, error: formik.errors[name], helperText: formik.errors[name]}
+}
+
+/**
+ * The form that handles adding Book entities to the system.
+ * @note This form is not for the sale pages, just the definition of a book, with title author etc.
+ */
+export default function AddBookForm() {
+    const formik = useFormik({
+        initialValues: {
+            bookTitle: "",
+            author: "",
+            publisher: "",
+            publishDate: "",
+            isbn: "",
+            coverArtURL: "",
+            tableOfContents: "",
+        },
+        onSubmit() {},
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <TextField {...inputProps(formik, "Book Title", "bookTitle")} />
+            <TextField {...inputProps(formik, "Author", "author")} />
+            <TextField {...inputProps(formik, "Publisher", "publisher")} />
+            <TextField {...inputProps(formik, "Publish Date", "publishDate")} type="date" />
+            <TextField {...inputProps(formik, "ISBN", "isbn")} />
+            <TextField {...inputProps(formik, "Table of Contents", "tableOfContents")} multiline />
+        </form>
+    )
+}
