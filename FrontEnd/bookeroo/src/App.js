ad4447acd7d5e390592ff40996a2feea90ee6c9b
jh0l <me@jh0l.com>
2021-08-23 17:25:05 +1000

    formatting

--- FrontEnd/bookeroo/src/App.js
+++ FrontEnd/bookeroo/src/App.js
@@ -1,19 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 // import { Router } from 'react-router-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Register from './components/UserManagement/Register'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from './components/UserManagement/Register';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-
-        <Route exact path="/register" component={Register} />
-
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Route exact path="/register" component={Register} />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
