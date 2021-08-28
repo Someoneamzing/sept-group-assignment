80f2efdb326cd9f651ceb6aeb2862e99f6d06483
jh0l <me@jh0l.com>
2021-08-29 03:56:15 +1000

    feat: :sparkles: add 404 page to routing

--- FrontEnd/bookeroo/src/App.js
+++ FrontEnd/bookeroo/src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import Header from './components/Layout/Header';
 import Contact from './components/Layout/Contact';
 import ViewBook from './components/BookManagement/ViewBook';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, useLocation} from 'react-router-dom';
 import './App.css';
 import Register from './components/UserManagement/Register';
+
+function NoMatch() {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h1>404</h1>
+            <h3>
+                No match for <code>{location.pathname}</code>
+            </h3>
+        </div>
+    );
+}
 
 function App() {
     return (
@@ -15,6 +28,9 @@ function App() {
                     <Route exact path="/register" component={Register} />
                     <Route exact path="/contact" component={Contact} />
                     <Route path="/books/:bookId" component={ViewBook} />
+                    <Route path="*">
+                        <NoMatch />
+                    </Route>
                 </Switch>
             </div>
         </div>
