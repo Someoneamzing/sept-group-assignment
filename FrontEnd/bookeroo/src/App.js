40b99391b680c4d876e90cd524092868203e1ebc
jh0l <me@jh0l.com>
2021-08-30 22:12:23 +1000

    refactor: :recycle: move 404 component to own file

--- FrontEnd/bookeroo/src/App.js
+++ FrontEnd/bookeroo/src/App.js
@@ -2,24 +2,13 @@ import React from 'react';
 import Header from './components/Layout/Header';
 import Contact from './components/Layout/Contact';
 import ViewBook from './components/BookManagement/ViewBook';
-import {Route, Switch, useLocation} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import Register from './components/UserManagement/Register';
 import {Container} from '@material-ui/core';
 import AddBookForSaleForm from './components/AddBookForSaleForm';
-
-function NoMatch() {
-    let location = useLocation();
-
-    return (
-        <div>
-            <h1>404</h1>
-            <h3>
-                No match for <code>{location.pathname}</code>
-            </h3>
-        </div>
-    );
-}
+import NoMatch from './components/Layout/NoMatch';
+import ViewBookForSalePage from './components/BookManagement/ViewBookForSale';
 
 function App() {
     return (
@@ -27,18 +16,22 @@ function App() {
             <Header />
             <div>
                 <Switch>
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/contact" component={Contact} />
-                    <Route
-                        exact
-                        path="/books/new"
-                        component={AddBookForSaleForm}
-                    />
-                    <Route path="/books/:bookId" component={ViewBook} />
                     <Route
                         exact
                         path="/"
                         children={<Container>HomePage</Container>}
+                    />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route
+                        exact
+                        path="/book/new"
+                        component={AddBookForSaleForm}
+                    />
+                    <Route path="/book/:bookId" component={ViewBook} />
+                    <Route
+                        path="/s/:sellerId/book/:bookId"
+                        component={ViewBookForSalePage}
                     />
                     <Route path="*">
                         <NoMatch />
---
+++ FrontEnd/bookeroo/src/components/Layout/NoMatch.js
@@ -0,0 +1,16 @@
+import {Box} from '@material-ui/core';
+import React from 'react';
+import {useLocation} from 'react-router-dom';
+
+export default function NoMatch() {
+    let location = useLocation();
+
+    return (
+        <Box margin="1rem">
+            <h1>404</h1>
+            <h3>
+                No match for <code>{location.pathname}</code>
+            </h3>
+        </Box>
+    );
+}
