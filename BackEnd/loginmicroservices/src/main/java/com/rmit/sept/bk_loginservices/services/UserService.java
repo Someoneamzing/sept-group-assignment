e94b9a71f2e6d8d471c3c6d476c27c9b61060e2a
jh0l <me@jh0l.com>
2021-09-06 18:21:16 +1000

    Update UserService.java

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/services/UserService.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/services/UserService.java
@@ -4,43 +4,57 @@ package com.rmit.sept.bk_loginservices.services;
 
 
 import com.rmit.sept.bk_loginservices.Repositories.UserRepository;
-import com.rmit.sept.bk_loginservices.exceptions.UsernameAlreadyExistsException;
+import com.rmit.sept.bk_loginservices.Repositories.UserTypeRepository;
+import com.rmit.sept.bk_loginservices.exceptions.ConstraintViolationException;
 import com.rmit.sept.bk_loginservices.model.User;
+import com.rmit.sept.bk_loginservices.model.UserType;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    protected final Log logger = LogFactory.getLog(getClass());
+
+
+    private final UserRepository userRepository;
+
+    private final UserTypeRepository userTypeRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, UserTypeRepository userTypeRepository) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userTypeRepository = userTypeRepository;
+    }
 
-    public User saveUser (User newUser){
+    public User saveNewUser(User newUser){
 
-      /*  newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
         //Username has to be unique (exception)
-        // Make sure that password and confirmPassword match
         // We don't persist or show the confirmPassword
-        return userRepository.save(newUser);
-       */
-//        try{
         newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
         //Username has to be unique (exception)
         newUser.setUsername(newUser.getUsername());
-        // Make sure that password and confirmPassword match
+        // User authorities are by default none
+        Set<UserType> auths = new HashSet<>();
+        auths.add(userTypeRepository.getPublic());
+        newUser.setAuthorities(auths);
         // We don't persist or show the confirmPassword
         newUser.setConfirmPassword("");
+        try {
         return userRepository.save(newUser);
-
-//        }catch (Exception e){
-//            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
-//        }
-
+        }catch (Exception e){
+            logger.error(e);
+            throw new ConstraintViolationException("A constraint was not properly validated, the error has been logged.");
+        }
     }
-
-
 }
