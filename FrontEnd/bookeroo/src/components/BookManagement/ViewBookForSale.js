4f1c239a908f41d5a6f567e71586fe24c6107018
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2021-10-11 19:46:24 +1100

    feat(frontend): :sparkles: Implement add to order behaviour

--- FrontEnd/bookeroo/src/components/BookManagement/ViewBookForSale.js
+++ FrontEnd/bookeroo/src/components/BookManagement/ViewBookForSale.js
@@ -1,16 +1,46 @@
-import {Box, Button, TextField} from '@material-ui/core';
-import React, {Suspense} from 'react';
-import {useRecoilValue} from 'recoil';
+import {Box, Button, CircularProgress, TextField} from '@material-ui/core';
+import React, {Suspense, useEffect, useState} from 'react';
+import {useRecoilValue, useSetRecoilState} from 'recoil';
 import {bookAtomFamily} from '../../state/books/books';
 import {useBookForSale} from '../../state/books/booksForSale';
 import {ViewBookLayout} from './ViewBook';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import DoneIcon from '@material-ui/icons/Done';
 import NoMatch from '../Layout/NoMatch';
 import {useParams} from 'react-router';
 import CreateBook from './_DEBUG_components/CreateBook';
 import Reviews from './Reviews';
+import {idFromURL} from '../../state/utils';
+import {createOrderItem} from '../../api';
+import {userAtom} from '../../state/user/authentication';
 
-function PurchaseLayout({sellPriceInCents, availableStock}) {
+function PurchaseLayout({sellPriceInCents, availableStock, _links}) {
+    const [loading, setLoading] = useState();
+    const [success, setSuccess] = useState();
+    const [quantity, setQuantity] = useState(1);
+    const user = useRecoilValue(userAtom);
+    const bookForSaleId = idFromURL(_links.self.href);
+    // Reset success to false after 5 seconds.
+    useEffect(() => {
+        let timer = null;
+        if (success) {
+            timer = setTimeout(() => {
+                timer = null;
+                setSuccess(false);
+            }, 5000);
+        }
+        return () => timer && clearTimeout(timer);
+    }, [success]);
+
+    async function handleAddButtonClick() {
+        setSuccess(false);
+        setLoading(true);
+        await createOrderItem({
+            item: {costInCents: sellPriceInCents, bookForSaleId, quantity},
+            token: user.token,
+        });
+    }
+
     return (
         <Box
             display="flex"
@@ -24,17 +54,38 @@ function PurchaseLayout({sellPriceInCents, availableStock}) {
                 <b>{availableStock || '?'}</b> units left
             </div>
             <div>
-                <TextField id="standard-basic" label="Quantity" type="number" />
+                <TextField
+                    id="standard-basic"
+                    label="Quantity"
+                    type="number"
+                    value={quantity}
+                    onChange={(e) => {
+                        setQuantity(e.target.value);
+                    }}
+                />
             </div>
             <div>
                 <Button
                     variant="contained"
                     color="primary"
                     size="large"
-                    onClick={() => alert("this doesn't do anything lol")}
+                    onClick={handleAddButtonClick}
                 >
-                    Add to Order <pre> </pre>
-                    <AddShoppingCartIcon />
+                    {loading ? (
+                        <CircularProgress />
+                    ) : success ? (
+                        <>
+                            Successfully
+                            <br />
+                            Added<pre> </pre>
+                            <DoneIcon />
+                        </>
+                    ) : (
+                        <>
+                            Add to Order <pre> </pre>
+                            <AddShoppingCartIcon />
+                        </>
+                    )}
                 </Button>
             </div>
         </Box>
