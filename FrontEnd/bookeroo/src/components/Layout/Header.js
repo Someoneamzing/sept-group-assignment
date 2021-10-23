7aee3eb68c24c09532ebd9fc534de5d22634d9c2
Ming <s3843573@student.rmit.edu.au>
2021-10-23 18:14:31 +1100

    remove the underline for menu items and show books by filter button

--- FrontEnd/bookeroo/src/components/Layout/Header.js
+++ FrontEnd/bookeroo/src/components/Layout/Header.js
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
     },
     menuButton: {
         marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
+        [theme.breakpoints.up('md')]: {
             display: 'none',
         },
     },
@@ -63,14 +63,13 @@ const defaultLinks = {
 };
 
 const authedLinks = {
-    Logout: '/logout',
-    Users: '/users',
     Profile: '/users/profile',
+    Logout: '/logout',
 };
 
 const businessLinks = {Store: '/store'};
 
-const adminLinks = {Admin: '/admin'};
+const adminLinks = {Admin: '/users'};
 
 const unauthedLinks = {
     Register: '/register',
@@ -114,7 +113,10 @@ function Header() {
                         onClick={handleDrawerToggle}
                     >
                         <ListItem key={menuTitle} button>
-                            <ListItemText primary={menuTitle} />
+                            <ListItemText
+                                primary={menuTitle}
+                                className={classes.menuLink}
+                            />
                         </ListItem>
                     </Link>
                 ))}
@@ -146,10 +148,14 @@ function Header() {
                     </Link>
 
                     {/* Menu for Desktop*/}
-                    <Hidden xsDown implementation="css">
+                    <Hidden smDown implementation="css">
                         {Object.entries(menuItems).map(
                             ([menuTitle, pageURL]) => (
-                                <Link to={pageURL} key={menuTitle}>
+                                <Link
+                                    to={pageURL}
+                                    key={menuTitle}
+                                    style={{textDecoration: 'none'}}
+                                >
                                     <Button className={classes.menuItemDesktop}>
                                         {menuTitle}
                                     </Button>
